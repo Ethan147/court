8e35deeb02af456034d8fe963d8d160171e80276
Ethan Miller <ethan.mill147@gmail.com>
2023-03-25 13:32:54 -0600

    switch to ThreadedConnectionPool, update todo

--- README_TODO.md
+++ README_TODO.md
@@ -1,13 +1,14 @@
 ## Long term
--  [ ] generalize the sqlalchemy.url in alembic.ini
 
 ## Medium term
-- [ ] connection pooling in database.py
-- [ ] cursor in database.py
+- [x] connection pooling in database.py
+- [x] cursor in database.py
+- [ ] make pool.ThreadedConnectionPool flask-compliant
 
 ## Short term
 - [x] dbmate setup
 - [x] db connection via .env variables (12 factor)
-- [ ] decide on and setup a unit testing solution
+- [x] decide on and setup a unit testing solution
 - [ ] dbmate unit test setup (if applicable)
 - [ ] unit test coverage report (start this early, maybe as part of pre-commit)
+- [ ] start setting up flask
--- court/utils/db.py
+++ court/utils/db.py
@@ -1,23 +1,26 @@
 from typing import Any, Optional
 
-import psycopg2
+from psycopg2 import pool
 from psycopg2.extensions import connection, cursor
 
 from court.utils.env_conf import db_host, db_name, db_pass, db_port, db_user
 
+pool = pool.ThreadedConnectionPool(
+    minconn=1,
+    maxconn=10,
+    database=db_name,
+    user=db_user,
+    password=db_pass,
+    host=db_host,
+    port=db_port
+)
 
 def get_connection() -> connection:
-    conn = psycopg2.connect(
-      database=db_name,
-      user=db_user,
-      password=db_pass,
-      host=db_host,
-      port=db_port,
-    )
-    return conn
+    return pool.getconn()
 
 
 class Cursor:
+    """Base cursor for use across application"""
     def __init__(self) -> None:
         self.conn = get_connection()
 
@@ -30,10 +33,11 @@ class Cursor:
             exc_val: Optional[Any],
             exc_tb: Optional[Any]) -> None:
         self.conn.commit()
-        self.conn.close()
+        pool.putconn(self.conn)
 
 
 class CursorTest:
+    """Cursor for use by all test cases"""
     def __init__(self) -> None:
         self.conn = get_connection()
 
@@ -46,4 +50,4 @@ class CursorTest:
             exc_val: Optional[Any],
             exc_tb: Optional[Any]) -> None:
         self.conn.rollback()
-        self.conn.close()
+        pool.putconn(self.conn)
