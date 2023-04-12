d71a3f59ff667b44e86ea168b97e560a394c0ca6
Ethan Miller <ethan.mill147@gmail.com>
2023-04-12 17:57:31 -0600

    simple first name entry with button

--- expo_app/App.tsx
+++ expo_app/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
-import { Formik, Form, useFormik, FormikProvider } from "formik";
+import {
+  Button,
+  Provider as PaperProvider,
+  TextInput,
+} from "react-native-paper";
+import { Formik, useFormik, FormikProvider } from "formik";
+import * as Yup from "yup";
 
 const styles = StyleSheet.create({
   container: {
@@ -21,17 +26,37 @@ const styles = StyleSheet.create({
   },
 });
 
+const validationSchema = Yup.object().shape({
+  first_name: Yup.string().required("First name is required"),
+});
+
 const MyForm = () => {
   const formik = useFormik({
-    initialValues: {},
-    onSubmit: () => {},
+    initialValues: { first_name: "" },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("Submitted values:", values);
+    },
   });
 
   return (
     <FormikProvider value={formik}>
       <View>
-        <Text>some form</Text>
+        <TextInput
+          label="First Name"
+          value={formik.values.first_name}
+          onChangeText={formik.handleChange("first_name")}
+          onBlur={formik.handleBlur("first_name")}
+          error={!!formik.touched.first_name && !!formik.errors.first_name}
+          autoCompleteType={undefined}
+        />
+        {formik.touched.first_name && formik.errors.first_name ? (
+          <Text style={styles.error}>{formik.errors.first_name}</Text>
+        ) : null}
       </View>
+      <Button mode="contained" onPress={formik.handleSubmit}>
+        Submit
+      </Button>
     </FormikProvider>
   );
 };
