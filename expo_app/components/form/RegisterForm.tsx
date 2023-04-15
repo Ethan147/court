2dc3cf88f2395a7eeaab9ba1a3070a0821ba4be2
Ethan Miller <ethan.mill147@gmail.com>
2023-04-15 11:43:02 -0600

    last name add-in, cross platform spacing

--- expo_app/components/form/RegisterForm.tsx
+++ expo_app/components/form/RegisterForm.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import {
   View,
   StyleSheet,
   useWindowDimensions,
   Platform,
   Text,
 } from "react-native";
-import TextInputComp from "./basic/TextInputComp";
-
 import { Formik, useFormik, FormikProvider } from "formik";
 import * as Yup from "yup";
 import colors from "../../utils/colors";
+import TextInputComp from "./basic/TextInputComp";
 
 const validationSchema = Yup.object().shape({
-  first_name: Yup.string().required("First name is required"),
+  firstName: Yup.string().required("first name is required"),
+  lastName: Yup.string().required("last name is required"),
 });
 
 const RegisterForm = () => {
@@ -25,24 +29,28 @@ const RegisterForm = () => {
       justifyContent: "center",
       alignItems: "center",
     },
-    error: {
+    inputContainer: {
       ...Platform.select({
         web: {
-          // todo
+          marginBottom: windowDimensions.width * 0.02,
+          width: windowDimensions.width * 0.8,
         },
         ios: {
-          // todd
+          marginBottom: wp("2%"),
+          width: wp("80%"),
         },
         android: {
           // todo
         },
       }),
+    },
+    error: {
       color: colors.error,
     },
   });
 
   const formik = useFormik({
-    initialValues: { firstName: "" },
+    initialValues: { firstName: "", lastName: "" },
     validationSchema,
     onSubmit: (values) => {
       console.log("Submitted values:", values);
@@ -52,17 +60,31 @@ const RegisterForm = () => {
   return (
     <FormikProvider value={formik}>
       <View style={styles.container}>
-        <TextInputComp
-          label="First Name"
-          value={formik.values.firstName}
-          onChangeText={formik.handleChange("firstName")}
-          onBlur={formik.handleBlur("firstName")}
-          errors={!!formik.touched.firstName && !!formik.errors.firstName}
-        />
+        <View style={styles.inputContainer}>
+          <TextInputComp
+            label="first name"
+            value={formik.values.firstName}
+            onChangeText={formik.handleChange("firstName")}
+            onBlur={formik.handleBlur("firstName")}
+            errors={!!formik.touched.firstName && !!formik.errors.firstName}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <Text style={styles.error}>{formik.errors.firstName}</Text>
+          ) : null}
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInputComp
+            label="last name"
+            value={formik.values.lastName}
+            onChangeText={formik.handleChange("lastName")}
+            onBlur={formik.handleBlur("lastName")}
+            errors={!!formik.touched.lastName && !!formik.errors.lastName}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <Text style={styles.error}>{formik.errors.lastName}</Text>
+          ) : null}
+        </View>
       </View>
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <Text style={styles.error}>{formik.errors.firstName}</Text>
-      ) : null}
     </FormikProvider>
   );
 };
