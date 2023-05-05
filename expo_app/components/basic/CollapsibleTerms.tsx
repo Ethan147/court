9b71b37158ad0c850aba2f473ff127ba3183a993
Ethan Miller <ethan.mill147@gmail.com>
2023-05-05 17:46:22 -0600

    terms and conditions / private policy updates (in progress)

--- expo_app/components/basic/CollapsibleTerms.tsx
+++ expo_app/components/basic/CollapsibleTerms.tsx
@@ -1,156 +1,123 @@
-import React, { Component } from "react";
-import Accordion from "react-native-collapsible/Accordion";
-import {
-  View,
-  Text,
-  ViewStyle,
-  StyleProp,
-  TextStyle,
-  GestureResponderEvent,
-} from "react-native";
-import { CheckBox } from "react-native-elements";
-
-interface SectionData {
-  title: string;
-  content: string;
-}
-
-// todo get version information etc from API (to keep this up to date)
-const SECTIONS: SectionData[] = [
-  {
-    title: "Agree to Terms and Conditions",
-    content: `1. Introduction
-    Welcome to [app-name]! This app is designed to help you schedule games across singles and doubles, organize tournaments and challenge-ladders, find courts to play at, and maintain an internal ELO ranking system for players. The app also includes basic social media capabilities and geolocation features. By using our app, you agree to comply with these Terms of Service.
-
-    2. Privacy
-    We value your privacy and are committed to protecting your personal information. We will never sell your data to third parties. For more information about our data practices, please refer to our Privacy Policy.
-
-    3. User Conduct
-    As a user of our app, you are responsible for posting appropriate content. This includes, but is not limited to:
-
-    Respecting other users' privacy by not sharing their personal information without their consent
-    Refraining from posting lewd images or derogatory speech
-    Complying with all applicable laws and regulations
-    We reserve the right to remove any content that we deem inappropriate, and to suspend or terminate the accounts of users who violate these rules.
-
-    4. Geolocation
-    Our app utilizes geolocation technology to provide you with a more personalized experience. By using our app, you consent to the collection and use of your location data for this purpose.
-
-    5. Premium Features and Subscriptions
-    Access to certain premium features of our app requires payment via a subscription-based model. By subscribing to our premium services, you agree to pay the associated fees and abide by any additional terms that may apply to these features.
-
-    6. Intellectual Property
-    All content, features, and functionality within our app are owned by us and are protected by copyright, trademark, and other intellectual property laws. You are granted a limited, non-exclusive, non-transferable license to access and use the app for your personal, non-commercial purposes.
-
-    7. Limitation of Liability
-    We make no warranties, express or implied, regarding the availability, accuracy, or reliability of the app, its features, or its content. In no event shall we be liable for any damages, including but not limited to, direct, indirect, incidental, or consequential damages, arising out of your use of the app or your inability to use the app.
-
-    8. Changes to These Terms
-    We may update these Terms of Service from time to time. We will notify you of any significant changes and, where required by applicable law, obtain your consent to any such changes. Your continued use of our app following the posting of revised Terms of Service constitutes your acceptance of the updated terms.
-
-    9. Contact Us
-    If you have any questions or concerns regarding these Terms of Service or our app, please contact us at [your email address].`,
-  },
-];
+import React, { useState } from "react";
+import { View, Text, TextStyle, ViewStyle, StyleProp } from "react-native";
+import { List, Checkbox } from "react-native-paper";
 
 interface CollapsibleTermsProps {
-  passStyles: {
-    collapsibleTermsViewHeader: StyleProp<ViewStyle>;
-    collapsibleTermsHeaderText: StyleProp<ViewStyle>;
-    collapsibleTermsContent: StyleProp<TextStyle>;
-    collapsibleTermsContentText: StyleProp<TextStyle>;
+  acceptTerms?: boolean;
+  onPress?: () => void;
+  passStyles?: {
+    collapsibleTermsAccordionTitle?: StyleProp<TextStyle>;
+    collapsibleTermsAccordionContent?: StyleProp<TextStyle>;
+    collapsibleTermsCheckboxContainer?: StyleProp<ViewStyle>;
   };
 }
 
-interface CollapsibleTermsState {
-  activeSections: number[];
-}
-
-class AccordionComp extends Component<
-  CollapsibleTermsProps,
-  CollapsibleTermsState
-> {
-  state: CollapsibleTermsState = {
-    activeSections: [],
-  };
-
-  _renderHeader = (section: SectionData) => {
-    return (
-      <View style={this.props.passStyles.collapsibleTermsViewHeader}>
-        <Text style={this.props.passStyles.collapsibleTermsHeaderText}>
-          {section.title}
-        </Text>
-      </View>
-    );
-  };
-
-  _renderContent = (section: SectionData) => {
-    return (
-      <View style={this.props.passStyles.collapsibleTermsContent}>
-        <Text style={this.props.passStyles.collapsibleTermsContentText}>
-          {section.content}
-        </Text>
-      </View>
-    );
-  };
-
-  _updateSections = (activeSections: number[]) => {
-    this.setState({ activeSections });
-  };
-
-  render() {
-    return (
-      <Accordion
-        sections={SECTIONS}
-        activeSections={this.state.activeSections}
-        renderHeader={this._renderHeader}
-        renderContent={this._renderContent}
-        onChange={this._updateSections}
-      />
-    );
-  }
-}
-
-// todo: split out styles, clicking is an unpleasant experience at the moment
-const CollapsibleTerms = ({
-  acceptTerms,
+const CollapsibleTerms: React.FC<CollapsibleTermsProps> = ({
+  acceptTerms = false,
   onPress,
   passStyles,
-}: {
-  acceptTerms?: boolean;
-  onPress?: (event: GestureResponderEvent) => void;
-  passStyles?: {
-    collapsibleTermsViewContainer?: StyleProp<ViewStyle>;
-    collapsibleTermsViewHeader?: StyleProp<ViewStyle>;
-    collapsibleTermsHeaderText?: StyleProp<ViewStyle>;
-    collapsibleTermsContent?: StyleProp<TextStyle>;
-    collapsibleTermsContentText?: StyleProp<TextStyle>;
-  };
 }) => {
   const styles = {
-    collapsibleTermsViewContainer:
-      passStyles?.collapsibleTermsViewContainer || {
-        flexDirection: "row",
-        alignItems: "center",
-      },
-    collapsibleTermsViewHeader: passStyles?.collapsibleTermsViewHeader || {},
-    collapsibleTermsHeaderText: passStyles?.collapsibleTermsHeaderText || {},
-    collapsibleTermsContent: passStyles?.collapsibleTermsContent || {},
-    collapsibleTermsContentText: passStyles?.collapsibleTermsContentText || {},
+    collapsibleTermsAccordionTitle:
+      passStyles?.collapsibleTermsAccordionTitle || {},
+    collapsibleTermsAccordionContent:
+      passStyles?.collapsibleTermsAccordionContent || {},
+    collapsibleTermsCheckboxContainer:
+      passStyles?.collapsibleTermsCheckboxContainer || {},
+  };
+
+  const [expanded, setExpanded] = useState(false);
+  const [checked, setChecked] = useState(acceptTerms);
+
+  const handlePress = () => {
+    setExpanded(!expanded);
+    if (onPress) {
+      onPress();
+    }
+  };
+
+  const handleCheckbox = () => {
+    setChecked(!checked);
   };
 
   return (
-    <View style={styles.collapsibleTermsViewContainer}>
-      <CheckBox
-        checked={acceptTerms}
-        onPress={onPress}
-        containerStyle={{
-          borderWidth: 0,
-          paddingHorizontal: 0,
-          backgroundColor: "transparent",
-        }}
-      />
-      <AccordionComp passStyles={styles} />
+    <View>
+      <View style={styles?.collapsibleTermsCheckboxContainer}>
+        <Checkbox
+          status={checked ? "checked" : "unchecked"}
+          onPress={handleCheckbox}
+        />
+        <List.Accordion
+          titleStyle={styles?.collapsibleTermsAccordionTitle}
+          title="Agree to Terms and Conditions"
+          expanded={expanded}
+          onPress={handlePress}
+        >
+          <Text style={styles?.collapsibleTermsAccordionContent}>
+            1. Introduction{"\n"}
+            Welcome to [app-name]! This app is designed to help you schedule
+            games across singles and doubles, organize tournaments and
+            challenge-ladders, find courts to play at, and maintain an internal
+            ELO ranking system for players. The app also includes basic social
+            media capabilities and geolocation features. By using our app, you
+            agree to comply with these Terms of Service.{"\n"}
+            {"\n"}
+            2. Privacy{"\n"}
+            We value your privacy and are committed to protecting your personal
+            information. We will never sell your data to third parties. For more
+            information about our data practices, please refer to our Privacy
+            Policy.{"\n"}
+            {"\n"}
+            3. User Conduct{"\n"}
+            As a user of our app, you are responsible for posting appropriate
+            content. This includes, but is not limited to:{"\n"}
+            Respecting other users' privacy by not sharing their personal
+            information without their consent{"\n"}
+            Refraining from posting lewd images or derogatory speech{"\n"}
+            Complying with all applicable laws and regulations{"\n"}
+            We reserve the right to remove any content that we deem
+            inappropriate, and to suspend or terminate the accounts of users who
+            violate these rules.{"\n"}
+            {"\n"}
+            4. Geolocation{"\n"}
+            Our app utilizes geolocation technology to provide you with a more
+            personalized experience. By using our app, you consent to the
+            collection and use of your location data for this purpose.{"\n"}
+            {"\n"}
+            5. Premium Features and Subscriptions{"\n"}
+            Access to certain premium features of our app requires payment via a
+            subscription-based model. By subscribing to our premium services,
+            you agree to pay the associated fees and abide by any additional
+            terms that may apply to these features.{"\n"}
+            {"\n"}
+            6. Intellectual Property{"\n"}
+            All content, features, and functionality within our app are owned by
+            us and are protected by copyright, trademark, and other intellectual
+            property laws. You are granted a limited, non-exclusive,
+            non-transferable license to access and use the app for your
+            personal, non-commercial purposes.{"\n"}
+            {"\n"}
+            7. Limitation of Liability{"\n"}
+            We make no warranties, express or implied, regarding the
+            availability, accuracy, or reliability of the app, its features, or
+            its content. In no event shall we be liable for any damages,
+            including but not limited to, direct, indirect, incidental, or
+            consequential damages, arising out of your use of the app or your
+            inability to use the app.{"\n"}
+            {"\n"}
+            8. Changes to These Terms{"\n"}
+            We may update these Terms of Service from time to time. We will
+            notify you of any significant changes and, where required by
+            applicable law, obtain your consent to any such changes. Your
+            continued use of our app following the posting of revised Terms of
+            Service constitutes your acceptance of the updated terms.{"\n"}
+            {"\n"}
+            9. Contact Us{"\n"}
+            If you have any questions or concerns regarding these Terms of
+            Service or our app, please contact us at [your email address].`,
+          </Text>
+        </List.Accordion>
+      </View>
     </View>
   );
 };
--- expo_app/components/registerForm/RegisterForm.tsx
+++ expo_app/components/registerForm/RegisterForm.tsx
@@ -21,6 +21,76 @@ import DatePickerComp from "../basic/DatePickerComp";
 import AddressInputComp from "../basic/AddressInputComp";
 import CollapsibleTerms from "../basic/CollapsibleTerms";
 
+// todo incorporated into components
+const TermsAndConditions = `1. Introduction
+Welcome to [app-name]! This app is designed to help you schedule games across singles and doubles, organize tournaments and challenge-ladders, find courts to play at, and maintain an internal ELO ranking system for players. The app also includes basic social media capabilities and geolocation features. By using our app, you agree to comply with these Terms of Service and our Privacy Policy.
+
+2. User Conduct
+As a user of our app, you are responsible for posting appropriate content. This includes, but is not limited to:
+
+Respecting other users' privacy by not sharing their personal information without their consent
+Refraining from posting lewd images or derogatory speech
+Complying with all applicable laws and regulations
+We reserve the right to remove any content that we deem inappropriate and to suspend or terminate the accounts of users who violate these rules.
+
+3. Geolocation
+Our app utilizes geolocation technology to provide you with a more personalized experience. By using our app, you consent to the collection and use of your location data for this purpose.
+
+4. Premium Features and Subscriptions
+Access to certain premium features of our app requires payment via a subscription-based model. By subscribing to our premium services, you agree to pay the associated fees and abide by any additional terms that may apply to these features.
+
+5. Intellectual Property
+All content, features, and functionality within our app are owned by us and are protected by copyright, trademark, and other intellectual property laws. You are granted a limited, non-exclusive, non-transferable license to access and use the app for your personal, non-commercial purposes.
+
+6. Limitation of Liability
+We make no warranties, express or implied, regarding the availability, accuracy, or reliability of the app, its features, or its content. In no event shall we be liable for any damages, including but not limited to, direct, indirect, incidental, or consequential damages, arising out of your use of the app or your inability to use the app.
+
+7. Changes to These Terms
+We may update these Terms of Service from time to time. We will notify you of any significant changes and, where required by applicable law, obtain your consent to any such changes. Your continued use of our app following the posting of revised Terms of Service constitutes your acceptance of the updated terms.
+
+8. Contact Us
+If you have any questions or concerns regarding these Terms of Service or our app, please contact us at [your email address].`;
+
+const PrivacyPolicy = `1. Introduction
+We value your privacy and are committed to protecting your personal information. This Privacy Policy explains how we collect, use, store, and share your personal data when using our app. By using our app, you agree to the terms of this Privacy Policy.
+
+2. Collection of Personal Data
+When you use our app, we may collect the following types of personal data:
+
+Name
+Email address
+Date of birth
+Address
+Geolocation data
+
+3. Use of Personal Data
+We use your personal data to provide and improve our app's features and services, such as:
+
+Scheduling games
+Organizing tournaments and challenge-ladders
+Finding courts to play at
+Maintaining an internal ELO ranking system
+Personalizing your experience based on your location
+
+4. Data Sharing and Disclosure
+We will never sell your data to third parties. However, we may share your personal data with trusted third-party service providers to help us provide and improve our app's services. We require these service providers to maintain the confidentiality of your personal data and use it only for the purposes of providing services on our behalf. They must also comply with all applicable laws and regulations related to data protection and privacy.
+
+5. Data Security
+We take the security of your personal data seriously and implement appropriate technical and organizational measures to protect it from unauthorized access, disclosure, alteration, or destruction. However, please be aware that no method of data transmission or storage is 100% secure, and we cannot guarantee the absolute security of your personal data.
+
+6. Data Retention
+We will retain your personal data for as long as necessary to fulfill the purposes for which it was collected, including for the purposes of satisfying any legal, accounting, or reporting requirements. While we aim to maintain the best of intentions when handling your data, please be aware that there might not be a specific need for our app to delete or anonymize your data, unless the data load becomes exceptionally large. In such cases, we will assess the situation and take appropriate measures as needed.
+
+7. Your Righst and Choices
+Depending on your jurisdiction, you may have certain rights related to your personal data, such as the right to access, correct, delete, or restrict the processing of your data. If you would like to exercise any of these rights, please contact us at [your email address]. We will respond to your request in accordance with applicable data protection laws.
+
+8. Changes to This Privacy Policy
+We may update this Privacy Policy from time to time. We will notify you of any significant changes and, where required by applicable law, obtain your consent to any such changes. Your continued use of our app following the posting of a revised Privacy Policy constitutes your acceptance of the updated policy.
+
+9. Contact Us
+If you have any questions or concerns regarding this Privacy Policy or our data practices, please contact us at [your email address].
+`;
+
 const RegisterForm = () => {
   const windowDimensions = useWindowDimensions();
 
@@ -205,6 +275,11 @@ const RegisterForm = () => {
     },
     toggleButtonGroupCompSelectedButton: {
       backgroundColor: colors.settingSelect,
+    },
+    // in progress
+    collapsibleTermsViewContainer: {
+      flexDirection: "row",
+      alignItems: "center",
     },
   });
 
