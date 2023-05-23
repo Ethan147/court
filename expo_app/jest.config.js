f65f92510d301883a36906f09ca9e532b0af7d38
Ethan Miller <ethan.mill147@gmail.com>
2023-05-22 18:40:09 -0600

    very rough unit testing setup

--- expo_app/jest.config.js
+++ expo_app/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: "jest-expo",
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+  },
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)",
