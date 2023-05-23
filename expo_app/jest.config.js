eb2bc96655dfb2d31b7dcb49f598f4db2c223040
Ethan Miller <ethan.mill147@gmail.com>
2023-05-22 18:22:37 -0600

    basic setup for react unit testing

---
+++ expo_app/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "jest-expo",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)",
+  ],
+  testMatch: ["<rootDir>/tests/**/*.test.tsx"],
+};
