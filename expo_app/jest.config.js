ebd823b736d79f7bf1e9c897488b9f51dd263cd4
Ethan Miller <ethan.mill147@gmail.com>
2023-05-24 18:13:19 -0600

    setup (mocking etc) to start RTL

--- expo_app/jest.config.js
+++ expo_app/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   preset: "jest-expo",
   transform: {
-    "\\.[jt]sx?$": "babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  setupFiles: ["./jest.setup.js"],
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)",
+    "node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@react-native/assets/.*|@react-native/polyfills/.*|react-native-jest/setup.js)",
   ],
   testMatch: ["<rootDir>/tests/**/*.test.tsx"],
 };
