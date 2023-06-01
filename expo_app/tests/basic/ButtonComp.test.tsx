6c579855aee68f0b0d1263a8f23ce825c85cd281
Ethan Miller <ethan.mill147@gmail.com>
2023-05-31 18:22:03 -0600

    test: renders without crash, renders correct test, calls the onPress function

--- expo_app/tests/basic/ButtonComp.test.tsx
+++ expo_app/tests/basic/ButtonComp.test.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 
 import ButtonComp from "../../components/basic/ButtonComp";
 
 describe("<ButtonComp />", () => {
-  it("has 1 child", () => {
+  it("renders without crashing", () => {
     const { getByTestId } = render(<ButtonComp />);
     expect(getByTestId("buttonComp")).toBeDefined();
+  });
+
+  it("renders the correct text when passed as a prop", () => {
+    const { getByText } = render(<ButtonComp text="Test button" />);
+    expect(getByText("Test button")).toBeDefined();
+  });
+
+  it("calls the onPress function when pressed", () => {
+    const onPressMock = jest.fn();
+    const { getByTestId } = render(<ButtonComp onPress={onPressMock} />);
+    const button = getByTestId("buttonComp");
+
+    fireEvent.press(button);
+    expect(onPressMock).toHaveBeenCalled();
   });
 });
