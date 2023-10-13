752eabc3fa9ad7e29c690ad17c8cb5b3b9e13f41
Ethan Miller <ethan.mill147@gmail.com>
2023-10-13 17:51:19 -0600

    draft starting tables, prepare unit tests

--- court/utils/aws_cognito.py
+++ court/utils/aws_cognito.py
@@ -4,9 +4,10 @@ Utilities for interacting with aws cognito
 from typing import Any, Dict
 
 import boto3
-from utils.env_conf import cognito_client_id
 
-client = boto3.client('cognito-idp')
+from court.utils.env_conf import cognito_client_id
+
+client = boto3.client('cognito-idp', region_name= 'us-east-2')
 
 def cognito_sign_up(body: Dict[str, Any]) -> Dict[str, Any]:
     return client.sign_up(
