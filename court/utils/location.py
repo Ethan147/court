8c73c7a1d8f9befddbdb5fe8ab3b890b30edf0bf
Ethan <ethan.mill147@gmail.com>
2023-11-14 19:21:18 -0700

    wip more user_play_location find or insert logic

--- court/utils/location.py
+++ court/utils/location.py
@@ -172,33 +172,58 @@ def _insert_user_mailing_address(
         is_active=True
     )
 
-def find_or_create_user_play_location() -> UserPlayLocation:
-    pass
-
-def find_user_play_location() -> Optional[UserPlayLocation]:
-    """
-    curs.execute(
-        "SELECT
-            ST_Y(ST_GeographyFromText(ST_AsText(location))) AS latitude,
-            ST_X(ST_GeographyFromText(ST_AsText(location))) AS longitude
-            FROM user_play_location WHERE id = %s",
-            (1,)
+def find_or_create_user_play_location(
+    user_account_id: int,
+    address_line_1: str,
+    address_line_2: str,
+    city: str,
+    state: str,
+    country: str,
+    postal_code: str,
+    latitude: float,
+    longitude: float,
+    is_active: bool = True
+) -> UserPlayLocation:
+    user_play_location = find_user_play_location(
+        user_account_id, address_line_1, address_line_2,
+        city, state, country, postal_code, is_active
     )
-    """
+
+    if not user_play_location:
+        user_play_location = insert_user_play_location(
+            user_account_id, address_line_1, address_line_2,
+            city, state, country, postal_code, latitude, longitude, is_active
+        )
+
+    return user_play_location
+
+def find_user_play_location(
+    user_account_id: int,
+    address_line_1: str,
+    address_line_2: str,
+    city: str,
+    state: str,
+    country: str,
+    postal_code: str,
+    is_active: bool = True
+) -> Optional[UserPlayLocation]:
     with CursorRollback() as curs:
         query = """
             select
-
-            id, user_acccount_id, address_line_1, address_line_2,
-                   city, state, country, postal_code, created_at, updated_at,
+                   id,
+                   user_account_id,
+                   address_line_1,
+                   address_line_2,
+                   city,
+                   state,
+                   country,
+                   postal_code,
+                   ST_Y(ST_GeographyFromText(ST_AsText(location))) AS latitude,
+                   ST_X(ST_GeographyFromText(ST_AsText(location))) AS longitude
+                   created_at,
+                   updated_at,
                    is_active
-
-    user_account_id, address_line_1, address_line_2, city, state,
-    country, postal_code, latitude, longitude, created_at, updated_at,
-    is_active
-
-
-              from public.user_mailing_address
+              from public.user_play_location
              where user_account_id = %s
                and address_line_1 = %s
                and address_line_2 = %s
@@ -214,7 +239,7 @@ def find_user_play_location() -> Optional[UserPlayLocation]:
         rows = curs.fetchall()
 
     if len(rows) > 1:
-        raise ValueError("Multiple users found")
+        raise ValueError("Multiple user play locations found")
 
     if len(rows) == 0:
         return None
@@ -229,11 +254,53 @@ def find_user_play_location() -> Optional[UserPlayLocation]:
         state=user_mailing_address[5],
         country=user_mailing_address[6],
         postal_code=user_mailing_address[7],
-        created_at=user_mailing_address[8],
-        updated_at=user_mailing_address[9],
-        is_active=user_mailing_address[10]
+        latitude=user_mailing_address[8],
+        longitude=user_mailing_address[9],
+        created_at=user_mailing_address[10],
+        updated_at=user_mailing_address[11],
+        is_active=user_mailing_address[12]
     )
 
 
-def insert_user_play_location() -> UserPlayLocation:
-    pass
+def insert_user_play_location(
+    user_account_id: int,
+    address_line_1: str,
+    address_line_2: str,
+    city: str,
+    state: str,
+    country: str,
+    postal_code: str,
+    latitude: float,
+    longitude: float,
+    is_active: bool = True
+) -> UserPlayLocation:
+    with CursorCommit() as curs:
+        query = """
+            insert into public.user_play_location
+            (user_account_id, address_line_1, city, state, country, postal_code, location, created_at)
+            values
+            (%s, %s, %s, %s, %s, %s, ST_SetSRID(ST_MakePoint(%s, %s), 4326), now())
+            returning id, created_at, updated_at
+        """
+        curs.execute(
+            query,
+            (user_account_id, address_line_1, address_line_2, city, state, country, postal_code, is_active)
+        )
+        user_play_location_id, created_at, updated_at = curs.fetchall()[0]
+
+
+    return UserPlayLocation(
+        id=user_play_location_id,
+        user_account_id=user_account_id,
+        address_line_1=address_line_1,
+        address_line_2=address_line_2,
+        city=city,
+        state=state,
+        country=country,
+        postal_code=postal_code,
+        latitude=latitude,
+        longitude=longitude,
+        created_at=created_at,
+        updated_at=updated_at,
+        is_active=True
+    )
