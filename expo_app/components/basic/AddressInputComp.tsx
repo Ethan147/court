e1e729b9576354867d5e4a4a5f5774f64b8ced8a
Ethan <ethan.mill147@gmail.com>
2023-11-30 19:34:51 -0700

    platform divided address component, api location updates

--- expo_app/components/basic/AddressInputComp.tsx
+++ expo_app/components/basic/AddressInputComp.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { StyleProp, View, ViewStyle } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, ViewStyle, Platform, StyleProp } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { debounce } from "lodash";
+import TextInputComp from "./TextInputComp";
 
 interface AddressInputCompProps {
   onPlaceSelected: (data: any, details: any) => void;
@@ -22,14 +24,44 @@ const AddressInputComp: React.FC<AddressInputCompProps> = ({
       passStyles?.addressInputCompGooglePlacesAutoCompete || {},
   };
 
+  const [suggestions, setSuggestions] = useState([]); // State to store suggestions
+
+  const fetchPlaces = (text: string) => {
+    fetch("https://your-backend-endpoint.com/google-places", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ input_text: text }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setSuggestions(data.predictions); // Assuming 'data.predictions' contains your suggestions
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  // 300 ms debounce
+  const debouncedFetchPlaces = debounce(fetchPlaces, 300);
+
+  useEffect(() => {
+    // Cleanup debounce
+    return () => debouncedFetchPlaces.cancel();
+  }, []);
+
+  if (Platform.OS === "web") {
+    return (
+      <TextInputComp
+        label="first name"
+        // value={formik.values.firstName}
+        // onChangeText={formik.handleChange("firstName")}
+        // onBlur={handleBlurOnlyIfNotEmpty("firstName")}
+        // error={!!formik.touched.firstName && !!formik.errors.firstName}
+        // passStyles={passTextInputTopStyles}
+      />
+    );
+  }
+
   return (
-    <View style={styles.addressInputCompViewContainer}>
-      {/* todo on mobile interacting with this raises an error message
-      "
-        VirtualizedLists should never be nested inside plain ScrollViews with the same orientation
-        because it can break windowing and other functionality - use another
-        VirtualizedList-backed container instead.
-      " */}
+    <View style={passStyles?.addressInputCompViewContainer}>
       <GooglePlacesAutocomplete
         placeholder="address"
         onPress={(data, details = null) => {
