ac798fabbf5d57a0f029fbda5a91e42e55f80890
Ethan <ethan.mill147@gmail.com>
2023-12-16 21:54:25 -0700

    add key to parameter store & first working response

--- court/lambdas/api_location/api_location.py
+++ court/lambdas/api_location/api_location.py
@@ -1,12 +1,18 @@
 import json
 import os
 
+import boto3
 import requests
 from pydantic import BaseModel, ValidationError
 
 
 class GooglePlacesRequest(BaseModel):
     input_text: str
+
+def get_api_key(parameter_name: str) -> str:
+    ssm = boto3.client('ssm')
+    parameter = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
+    return parameter['Parameter']['Value']
 
 def lambda_google_places_proxy(event, _):
     try:
@@ -24,19 +30,18 @@ def lambda_google_places_proxy(event, _):
         # Parse request body
         request_data = GooglePlacesRequest(**json.loads(event['body']))
 
-        # google_api_key = os.environ.get('apikey')
-        google_api_key = 'key_here'
+        google_api_key = get_api_key('google_places_api_key')
         google_places_url = f"https://maps.googleapis.com/maps/api/place/autocomplete/json?input={request_data.input_text}&key={google_api_key}"
 
         response_body = requests.get(google_places_url)
-        raise ValueError(response_body)
+        response_data = response_body.json()
+        serialized_response_data = json.dumps(response_data)
 
         # Static response for testing
         response = {
             "statusCode": 200,
             "headers": {"Access-Control-Allow-Origin": "*","Content-Type": "application/json"},
-            # "body": response_body
-            "body": {"this is my": "response"}
+            "body": serialized_response_data
         }
 
     except ValidationError as e:
