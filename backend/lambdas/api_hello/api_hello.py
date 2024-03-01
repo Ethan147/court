40de33b6ff2a6cfaa4f15a6ced91bbbfae5acc8c
Ethan <ethan.mill147@gmail.com>
2024-02-29 17:55:25 -0700

    simplistic API is now returning hello world

--- backend/lambdas/api_hello/api_hello.py
+++ backend/lambdas/api_hello/api_hello.py
@@ -1,26 +1,28 @@
+from typing import Any, Dict
+
 
 def lambda_hello(event: Dict, _: Any) -> Dict[str, Any]:
     """
     As a first request for a user,
     only gather the expected primary play location for the user
     """
-    try:
-        if event.get('httpMethod') == 'OPTIONS':
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Origin': '*',  # or specify your domain
-                    'Access-Control-Allow-Methods': 'POST',
-                    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-                },
-                'body': None
-            }
+    # try:
+    #     if event.get('httpMethod') == 'OPTIONS':
+    #         return {
+    #             'statusCode': 200,
+    #             'headers': {
+    #                 'Access-Control-Allow-Origin': '*',  # or specify your domain
+    #                 'Access-Control-Allow-Methods': 'POST',
+    #                 'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    #             },
+    #             'body': None
+    #         }
 
-    except ValidationError as e:
-        return {"statusCode": 400, "body": json.dumps({"message": str(e)})}
+    # except ValidationError as e:
+    #     return {"statusCode": 400, "body": json.dumps({"message": str(e)})}
 
-    except Exception as e:
-        return {"statusCode": 500, "body": json.dumps({"message": str(e)})}
+    # except Exception as e:
+    #     return {"statusCode": 500, "body": json.dumps({"message": str(e)})}
 
     return {
         'statusCode': 200,
